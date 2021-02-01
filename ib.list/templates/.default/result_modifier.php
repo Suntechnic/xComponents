e5cf34bcc336a7fabcebfe5292a4120bd2a513bf
madzhugin <madzhugin@yandex.ru>
2021-02-01 18:24:14 +0300

    добавление примеров в базовый шаблон ib.дшые

--- ib.list/templates/.default/result_modifier.php
+++ ib.list/templates/.default/result_modifier.php
@@ -1,8 +1,42 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// изображения
 foreach ($arResult['ITEMS'] as $i=>$dctItem) {
     $arResult['ITEMS'][$i]['PREVIEW_PICTURE'] = \CFile::getFileArray($dctItem['PREVIEW_PICTURE']);
     $arResult['ITEMS'][$i]['DETAIL_PICTURE'] = \CFile::getFileArray($dctItem['DETAIL_PICTURE']);
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// добавление ключей к кэшируемым
 $this->__component->setResultCacheKeys(['BROWSER_TITLE','BROWSER_COLOR']);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// получить базовый фильтр
+$arBaseFilter = $this->__component->getFilter();
+// например для того чтобы найти сосседей:
+$arSelect = Array(
+        'IBLOCK_ID',
+        'ID',
+        'NAME',
+        'DETAIL_PAGE_URL'
+    );
+
+$db_res = CIBlockElement::GetList(
+        $arParams['SORT'],
+        $this->__component->getFilter(),
+        false,
+        ['nElementID' => $arResult['ITEMS'][0]['ID'], 'nPageSize' => 1],
+        $arSelect
+    );
+$rel_rank = 0;
+while($arElement = $db_res->GetNext()) {
+    if ($arElement['ID'] != $arResult['ITEM']['ID']) {
+        $arResult['NEIGHBORS'][$rel_rank] = $arElement;
+    } else {
+        $rel_rank = 1;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// примененный фильтр можн ополучить из $arResult['FILTER']
