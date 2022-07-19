8f6efe9a13ef5ea5fe07c2c6b9a4cd7644277f97
Suntechnic <madzhugin@yandex.ru>
2022-07-19 12:10:00 +0300

    Вывод сео-информации

--- ib.list/templates/.default/component_epilog.php
+++ ib.list/templates/.default/component_epilog.php
@@ -4,4 +4,31 @@
 //\Bitrix\Main\Context::getCurrent()->getResponse()->setLastModified($arResult["ITEMS_TIMESTAMP_X"]);
 
 // установка заголовка
-//$APPLICATION->SetPageProperty('title', 'TITLE', ['COMPONENT_NAME' => $component->getName()]);
+//$dctSeo = $arResult['ITEM']['SEO'];
+//
+//if ($dctSeo['ELEMENT_META_TITLE']) {
+//    $APPLICATION->SetPageProperty('title', $dctSeo['ELEMENT_META_TITLE'], ['COMPONENT_NAME' => $component->getName()]);
+//} else {
+//    $APPLICATION->SetPageProperty('title', $arResult['ITEM']['NAME'], ['COMPONENT_NAME' => $component->getName()]);
+//}
+//
+//if ($dctSeo['ELEMENT_META_DESCRIPTION']) {
+//    $APPLICATION->SetPageProperty('description', $dctSeo['ELEMENT_META_DESCRIPTION'], ['COMPONENT_NAME' => $component->getName()]);
+//} else {
+//    $APPLICATION->SetPageProperty('description', $arResult['ITEM']['NAME'], ['COMPONENT_NAME' => $component->getName()]);
+//}
+
+
+$dctSeo = $arResult['SECTION']['SEO'];
+
+if ($dctSeo['SECTION_META_TITLE']) {
+    $APPLICATION->SetPageProperty('title', $dctSeo['SECTION_META_TITLE'], ['COMPONENT_NAME' => $component->getName()]);
+} else {
+    $APPLICATION->SetPageProperty('title', $arResult['SECTION']['NAME'], ['COMPONENT_NAME' => $component->getName()]);
+}
+
+if ($dctSeo['SECTION_META_DESCRIPTION']) {
+    $APPLICATION->SetPageProperty('description', $dctSeo['SECTION_META_DESCRIPTION'], ['COMPONENT_NAME' => $component->getName()]);
+} else {
+    $APPLICATION->SetPageProperty('description', $arResult['SECTION']['NAME'], ['COMPONENT_NAME' => $component->getName()]);
+}
